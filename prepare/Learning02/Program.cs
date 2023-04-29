b1545fb9a3e30f2ee1866830f0c0dcae61f5d968
machado95 <yeckapum@gmail.com>
2023-04-28 22:53:16 -0300

    Adding W02 code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Serasa Experian";
+        job1._jobTitle = "Backoffice Agent";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "São Carlos Marklin Suítes";
+        job2._jobTitle = "Recepcionista";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
+        
+        Resume myResume = new Resume();
+        myResume._name = "Carlos Machado";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayJobs();
+
     }
 }
